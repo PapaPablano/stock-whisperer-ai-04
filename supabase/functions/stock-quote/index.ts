eacd004988093f3eb8246ff71fcbe5387fdabd8c
PapaPablano <62714634+PapaPablano@users.noreply.github.com>
2025-11-09 19:52:21 -0600

    Add 'x-application-name' to allowed CORS headers in all stock API functions

--- supabase/functions/stock-quote/index.ts
+++ supabase/functions/stock-quote/index.ts
@@ -2,7 +2,7 @@ import { supabaseAdmin } from '../_shared/supabaseAdminClient.ts'
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-application-name',
 }
 
 const CACHE_PREFIX = 'quote'
